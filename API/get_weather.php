026105fa2285c134f5365d33e44f7c69a0c9b657
Natalua9 <148848234+Natalua9@users.noreply.github.com>
2024-09-28 06:07:39 +0300

    Add files via upload

---
+++ API/get_weather.php
@@ -0,0 +1,21 @@
+<?php
+// Ваш API-ключ
+$apiKey = "b8eb9a80346dc7f46f5a01aa21b6a094";
+
+// Функция для получения погоды по ID города
+function getWeather($cityId) {
+    global $apiKey;
+
+    $url = "https://api.openweathermap.org/data/2.5/weather?id={$cityId}&units=metric&appid={$apiKey}&lang=ru";
+
+    $response = file_get_contents($url);
+
+    return $response;
+}
+
+// Получаем ID города из запроса
+$cityId = $_GET['cityId'];
+
+// Возвращаем данные о погоде
+echo getWeather($cityId);
+?>
